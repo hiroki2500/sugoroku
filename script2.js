3121e0488bce97a666f51dc0786fd1158c4ab95b
hiroki maruyama <60976009+hiroki2500@users.noreply.github.com>
2020-04-17 09:04:41 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -8,15 +8,17 @@ let leftVal = 100; //残りのマス
 let diceMax = 6; //サイコロの目の最大値
 let diceMin = 1; //サイコロの目の最小値
 let diceImg; //サイコロの画像
-let moveNumMax = 4; //イベントで動く最大値
-let moveNumMin = 1; //イベントで動く最小値
+let forwardMoveNumMax = 4; //進むイベントで動く最大絶対値
+let forwardMoveNumMin = 1; //進むイベントで動く最小絶対値
+let backwardMoveNumMax = -4; //戻るイベントで動く最大絶対値
+let backwardMoveNumMin = -1; //戻るイベントで動く最小絶対値
 
 
 
 //初期画面
 document.getElementById(diceSumId).innerHTML = `<i class="fas fa-car-side"></i>`;
 
-//出目を取得+表示
+//出目をランダムに決定+表示
 function diceDisplay(){
   diceNum =  Math.floor( Math.random()*(diceMax +1 - diceMin)) + diceMin;
   $(`img`).attr(`src`, `${diceNum}.png`);
@@ -61,26 +63,27 @@ function forwardMotion(){
 //イベント発生時の動作
 function eventOccurrence(){
   if(document.getElementById(diceSumId).classList.contains('event')){
-    forwardEvent();
+    selectEvent();
   }
 };
 
+function selectEvent(){
+  selectNum = Math.floor( Math.random()*2)
+  if(selectNum === 1){forwardEvent()}
+  else{backwardEvent()}
+};
+
 //進むイベント
+//進むマスをランダムに決定＋表示
 function forwardEvent(){
-  moveNum =  Math.floor( Math.random()*(moveNumMax +1 - moveNumMin)) + moveNumMin;
-  $(`#eventInfo`).text(`イベント発生！${moveNum}マス進む`);
+  forwardMoveNum =  Math.floor( Math.random()*(forwardMoveNumMax +1 - forwardMoveNumMin)) + forwardMoveNumMin;
+  $(`#eventInfo`).text(`イベント発生！ +${forwardMoveNum}マス`);
   setTimeout(deleteInfo, 1300);
   setTimeout(forwardEventSquare, 1500);
 }
-
-
-//イベント発生の情報を消去
-function deleteInfo(){
-  $(`#eventInfo`).text(``);
-}
-
+//イベント時のコマの進む動作
 function forwardEventSquare(){
-  leftVal = leftVal - moveNum;
+  leftVal = leftVal - forwardMoveNum;
   diceSum = 100 -leftVal;
 
     //情報取得＋表示
@@ -92,6 +95,35 @@ function forwardEventSquare(){
     let element = document.getElementById('currentPosition');
     element.scrollIntoView({behavior: 'smooth',inline: 'center'}
   );
+}
+
+//戻るイベント
+//戻るマスをランダムに決定＋表示
+function backwardEvent(){
+  backwardMoveNum =  Math.floor( Math.random()*(backwardMoveNumMax +1 - backwardMoveNumMin)) + backwardMoveNumMin;
+  $(`#eventInfo`).text(`イベント発生！ ${backwardMoveNum}マス`);
+  setTimeout(deleteInfo, 1300);
+  setTimeout(backwardEventSquare, 1500);
+}
+//イベント時のコマの進む動作
+function backwardEventSquare(){
+  leftVal = leftVal - backwardMoveNum;
+  diceSum = 100 -leftVal;
+
+    //情報取得＋表示
+    $(`#leftValue`).text(`ゴールまであと${leftVal}マス`);
+    //コマの動作
+    document.getElementById(diceSumId).innerHTML = "";
+    diceSumId = ("square" + diceSum);
+    document.getElementById(diceSumId).innerHTML = `<i id="currentPosition" i class="fas fa-car-side"></i>`;
+    let element = document.getElementById('currentPosition');
+    element.scrollIntoView({behavior: 'smooth',inline: 'center'}
+  );
+}
+
+//イベント発生の情報を消去
+function deleteInfo(){
+  $(`#eventInfo`).text(``);
 }
 
 //サイコロを投げた時の処理

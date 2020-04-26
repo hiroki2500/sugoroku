8e4e3994dc7046230350392315e52222b7b0203d
hiroki maruyama <60976009+hiroki2500@users.noreply.github.com>
2020-04-26 11:45:59 +0900

    Update script3.js

--- script3.js
+++ script3.js
@@ -14,6 +14,7 @@ let diceSum2 = 0; //出目の合計
 let diceSumId2 = ("square" + diceSum2) //出目の合計値=コマの位置
 let leftVal2 = 100; //残りのマス
 
+let totalCountNum = 0; //ボタンを押した総回数
 let diceMax = 6; //サイコロの目の最大値
 let diceMin = 1; //サイコロの目の最小値
 let diceImg; //サイコロの画像
@@ -44,7 +45,6 @@ function diceDisplay2(){
 function forwardMotion1(){
   leftVal1 = leftVal1 - diceNum1;
   diceSum1 = 100 -leftVal1;
-  $(`span`).attr(`id`, `rollDice2`);
 
   if(diceSum1 < 100){
     //情報取得＋表示
@@ -71,7 +71,7 @@ function forwardMotion1(){
     diceSum1 = 100;
     diceSumId1 = ("square" + diceSum1);
     document.getElementById(diceSumId1).insertAdjacentHTML('afterbegin', '<i id="currentPosition1" class="fas fa-car-side playerIcon1"></i>');
-    let element = document.getElementById('currentPosition');
+    let element = document.getElementById('currentPosition1');
     element.scrollIntoView({behavior: 'smooth',inline: 'end'});
     }
   };
@@ -80,7 +80,6 @@ function forwardMotion1(){
 function forwardMotion2(){
   leftVal2 = leftVal2 - diceNum2;
   diceSum2 = 100 -leftVal2;
-  $(`span`).attr(`id`, `rollDice1`);
 
   if(diceSum2 < 100){
     //情報取得＋表示
@@ -89,7 +88,7 @@ function forwardMotion2(){
     $(`#leftValue`).text(`ゴールまであと${leftVal2}マス`);
 
     //コマの動作
-    document.getElementById(diceSumId2).innerHTML = document.getElementById(diceSumId2).innerHTML.replace('<i id="currentPosition1" class="fas fa-car-side playerIcon2"></i>','');
+    document.getElementById(diceSumId2).innerHTML = document.getElementById(diceSumId2).innerHTML.replace('<i id="currentPosition2" class="fas fa-car-side playerIcon2"></i>','');
     diceSumId2 = ("square" + diceSum2);
     document.getElementById(diceSumId2).insertAdjacentHTML('afterbegin', '<i id="currentPosition2" class="fas fa-car-side playerIcon2"></i>');
     let element = document.getElementById('currentPosition2');
@@ -107,22 +106,22 @@ function forwardMotion2(){
     diceSum2 = 100;
     diceSumId2 = ("square" + diceSum2);
     document.getElementById(diceSumId2).insertAdjacentHTML('afterbegin', '<i id="currentPosition2" class="fas fa-car-side playerIcon2"></i>');
-    let element = document.getElementById('currentPosition');
+    let element = document.getElementById('currentPosition2');
     element.scrollIntoView({behavior: 'smooth',inline: 'end'});
     }
   };
 
 
-
 //サイコロを投げた時の処理
-$(`#rollDice2`).click(function(){
-  diceDisplay2();
-  forwardMotion2();
-});
-
-$(`#rollDice1`).click(function(){
-  diceDisplay1();
-  forwardMotion1();
+$(`#rollDice`).click(function(){
+  totalCountNum ++;
+  if(totalCountNum % 2 !== 0){
+    diceDisplay1();
+    forwardMotion1();
+  }else{
+    diceDisplay2();
+    forwardMotion2();
+  }
 });
 
 });

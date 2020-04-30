4b29f3b2e9b10ed0690496a86aa561db075ac957
hiroki maruyama <60976009+hiroki2500@users.noreply.github.com>
2020-04-30 21:50:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 $(function(){
 //サイコロを投げた時の処理
-$(`.btn`).click(function(){
-  $(`.choice`).addClass(`none`);
-  $(`.game`).removeClass(`none`);
-});
+
 $(`#one`).click(function(){
+  location.href = "./index.html";
   $(`#selectScript`).attr(`src`, `script1.js`);
 });
 $(`#two`).click(function(){
+  location.href = "./index.html";
   $(`#selectScript`).attr(`src`, `script2.js`);
 });
 $(`#three`).click(function(){
+  location.href = "./index.html";
   $(`#selectScript`).attr(`src`, `script3.js`);
 });
 $(`#four`).click(function(){
+  location.href = "./index.html";
   $(`#selectScript`).attr(`src`, `script4.js`);
 });
 });
